00c075d0c98d99186fcdf339f8b9d871cfac2c68
Lee Yuseung <105223190+yuseung0429@users.noreply.github.com>
2024-10-16 00:49:39 +0900

    [Silver II] Title: 병사 배치하기, Time: 116 ms, Memory: 14572 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
@@ -3,28 +3,39 @@ import java.util.*;
 
 class Main {
 	
-	public static int solution(int[] arr){
-		ArrayList<Integer> list = new ArrayList<Integer>();
+	public static ArrayList<Integer> list;
+	
+	public static int solution(int[] arr) {
+		list = new ArrayList<Integer>();
 		list.add(arr[0]);
 		
-		for(int i=1; i<arr.length; i++){
-			int num = arr[i];
-			int size = list.size();
-			for(int j=size-1; j>=0; j--){
-				if(list.get(j) <= num){
-					if(j == 0)
-						list.set(j, num);
-					continue;
-				}
-				if(j+1 == size)
-					list.add(num);
-				else
-					list.set(j+1, num);
-				break;
-			}
+		for(int i=1; i<arr.length; i++) {
+			int idx = binarySearch(arr[i]);
+			
+			if(idx == list.size())
+				list.add(arr[i]);
+			
+			if(list.get(idx) < arr[i])
+				list.set(idx, arr[i]);
 		}
-		
 		return arr.length - list.size();
+	}
+	
+	public static int binarySearch(int num){
+		int left = 0;
+		int right = list.size()-1;
+	
+		while(left <= right) {
+			int mid = (left+right)/2;
+			int value = list.get(mid);
+			if(value == num) 
+				return mid;
+			if(value < num)
+				right = mid-1;
+			else
+				left = mid+1;
+		}
+		return left;
 	}
 	
 	public static void main(String[] args) throws IOException {
