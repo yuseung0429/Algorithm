72d275550f09fed232dbfbd203ac2559dde369c1
Yuseung Lee <105223190+yuseung0429@users.noreply.github.com>
2025-02-22 16:20:56 +0900

    [Gold V] Title: 내려가기, Time: 420 ms, Memory: 61240 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -4,27 +4,30 @@ import java.util.*;
 class Main {
 	
 	public static void solution(int[][] matrix, int n) {
-		int[][][] dp = new int[2][n][3];
+		int[][] dp = new int[2][3];
 		
 		for (int i=0; i<2; i++) {
 			for (int j=0; j<3; j++) {
-				dp[i][0][j] = matrix[0][j];
+				dp[i][j] = matrix[0][j];
 			}
 		}
 		
 		for (int i=1; i<n; i++) {
-			dp[0][i][0] = matrix[i][0] + Math.max(dp[0][i-1][0], dp[0][i-1][1]);
-			dp[0][i][1] = matrix[i][1] + Math.max(dp[0][i-1][0], Math.max(dp[0][i-1][1], dp[0][i-1][2]));
-			dp[0][i][2] = matrix[i][2] + Math.max(dp[0][i-1][1], dp[0][i-1][2]);
+			int[] prevMax = dp[0].clone();
+			int[] prevMin = dp[1].clone();
 			
-			dp[1][i][0] = matrix[i][0] + Math.min(dp[1][i-1][0], dp[1][i-1][1]);
-			dp[1][i][1] = matrix[i][1] + Math.min(dp[1][i-1][0], Math.min(dp[1][i-1][1], dp[1][i-1][2]));
-			dp[1][i][2] = matrix[i][2] + Math.min(dp[1][i-1][1], dp[1][i-1][2]);
+			dp[0][0] = matrix[i][0] + Math.max(prevMax[0], prevMax[1]);
+			dp[0][1] = matrix[i][1] + Math.max(prevMax[0], Math.max(prevMax[1], prevMax[2]));
+			dp[0][2] = matrix[i][2] + Math.max(prevMax[1], prevMax[2]);
+			
+			dp[1][0] = matrix[i][0] + Math.min(prevMin[0], prevMin[1]);
+			dp[1][1] = matrix[i][1] + Math.min(prevMin[0], Math.min(prevMin[1], prevMin[2]));
+			dp[1][2] = matrix[i][2] + Math.min(prevMin[1], prevMin[2]);
 		}
 		
-		System.out.print(Math.max(dp[0][n-1][0], Math.max(dp[0][n-1][1], dp[0][n-1][2])));
+		System.out.print(Math.max(dp[0][0], Math.max(dp[0][1], dp[0][2])));
 		System.out.print(" ");
-		System.out.print(Math.min(dp[1][n-1][0], Math.min(dp[1][n-1][1], dp[1][n-1][2])));
+		System.out.print(Math.min(dp[1][0], Math.min(dp[1][1], dp[1][2])));
 	}
 	
 	public static void main(String[] args) throws Exception {
